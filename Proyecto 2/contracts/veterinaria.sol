bce445cecbfc3710efa22078feb470f176ae3bd3
Manuel Ferreras <35044975+ManuelFerreras@users.noreply.github.com>
2022-01-03 19:21:28 -0300

    conventional variables naming

--- Proyecto 2/contracts/veterinaria.sol
+++ Proyecto 2/contracts/veterinaria.sol
@@ -24,14 +24,33 @@ contract veterinaria {
 
     // Mapping Para Guardar la Informacion de los Servicios.
     mapping(string => Servicio) servicios;
+
     // Array Para Guardar los Nombres de los Servicios.
     string[] nombreServicios;
+
+
+    // Struct Para Crear Mascotas.
+    struct Mascota {
+        string tipoMascota;
+        string nombreMascota;
+        string apellidoMascota;
+    }
+
+    // Arreglo de las Mascotas Creadas.
+    Mascota[] mascotas;
+
+    // Mapping de las Mascotas de una Direccion.
+    mapping(address => uint[]) mascotasDeDireccion;
+
+    // Mapping para los servicios tomados por mascota.
+    mapping(uint => string[]) serviciosTomadosPorMascota;
 
 
     // Eventos
     event nuevoServicio(string);
     event servicioTomado(address, uint);
-    event nuevaMascota(string, uint);
+    event servicioDesactivado(string);
+    event nuevaMascota(string, string, address, uint);
 
 
     // Modificadores
@@ -45,23 +64,110 @@ contract veterinaria {
 
     }
 
+    modifier soloElDuenioDeMascota(uint _idMascota) {
 
-    function crearServicio(string memory nombreServicio_, string memory descripcionServicio_, uint256 precio_) public soloElDuenio {
+        // Chequeamos que el que envia la transaccion sea el duenio de la mascota.
+        bool _duenio = false;
 
-        // Chequeamos que la información ingresada sea correcta.
-        require(keccak256(abi.encodePacked(nombreServicio_)) != keccak256(abi.encodePacked("")), "El Nombre del servicio no puede estar vacio.");
-        require(keccak256(abi.encodePacked(descripcionServicio_)) != keccak256(abi.encodePacked("")), "La descripcion del servicio no puede estar vacio.");
-        require(precio_ > 0, "El precio debe ser mayor a 0");
+        // Chequeamos si la mascota esta en la lista del duenio.
+        for(uint256 i = 0; i < mascotasDeDireccion[msg.sender].length; i++) {
+            if (mascotasDeDireccion[msg.sender][i] == _idMascota) {
+                _duenio = true;
+            }
+        }
 
-        // Creamos el nuevo servicio.
-        servicios[nombreServicio_] = Servicio(nombreServicio_, descripcionServicio_, precio_, true);
-        // Agregamos el nombre del servicio al arreglo de nombres.
-        nombreServicios.push(nombreServicio_);
+        // Chequeamos si es el duenio.
+        require(_duenio == true, "No eres el duenio de la mascota.");
 
-        // Emitimos el evento correspondiente.
-        emit nuevoServicio(nombreServicio_);
+        // Finalizamos el Modifier.
+        _;
+        
+    }
+
+    modifier soloServicioActivo(string memory _nombreServicio) {
+
+        // Chequeamos que el servicio este activo.
+        require(servicios[_nombreServicio].estadoServicio, "El servicio deseado no se encuentra disponible.");
+
+        // Finalizamos el modifier.
+        _;
 
     }
 
+
+    function crearServicio(string memory _nombreServicio, string memory _descripcionServicio, uint256 _precio) public soloElDuenio {
+
+        // Chequeamos que la información ingresada sea correcta.
+        require(keccak256(abi.encodePacked(_nombreServicio)) != keccak256(abi.encodePacked("")), "El Nombre del servicio no puede estar vacio.");
+        require(keccak256(abi.encodePacked(_descripcionServicio)) != keccak256(abi.encodePacked("")), "La descripcion del servicio no puede estar vacio.");
+        require(_precio > 0, "El precio debe ser mayor a 0");
+
+        // Creamos el nuevo servicio.
+        servicios[_nombreServicio] = Servicio(_nombreServicio, _descripcionServicio, _precio, true);
+        // Agregamos el nombre del servicio al arreglo de nombres.
+        nombreServicios.push(_nombreServicio);
+
+        // Emitimos el evento correspondiente.
+        emit nuevoServicio(_nombreServicio);
+
+    }
+
+
+    function desactivarServicio(string memory _nombreServicio) public soloElDuenio {
+
+        // Chequeamos que el servicio este activo.
+        require(servicios[_nombreServicio].estadoServicio, "El servicio ya ha sido desactivado.");
+
+        // Desactivamos el servicio.
+        servicios[_nombreServicio].estadoServicio = false;
+
+        // Emitimos el evento correspondiente.
+        emit servicioDesactivado(_nombreServicio);
+
+    }
+
+
+    function crearMascota(string memory _tipoMascota, string memory _nombreMascota, string memory _apellidoMascota) public {
+
+        // Creamos la mascota.
+        mascotas.push(Mascota(_tipoMascota, _nombreMascota, _apellidoMascota));
+
+        // Agregamos el id de la mascota a las mascotas de la direccion.
+        mascotasDeDireccion[msg.sender].push(mascotas.length - 1);
+
+        // Emitimos el evento correspondiente.
+        emit nuevaMascota(_tipoMascota, _nombreMascota, msg.sender, mascotas.length - 1);
+
+    }
+
+
+    function tomarServicio(string memory _nombreServicio, uint _idMascota) public payable soloElDuenioDeMascota(_idMascota) soloServicioActivo(_nombreServicio) {
+
+        // Chequeamos que se haya enviado la cantidad de ether necesaria.
+        require(msg.value >= servicios[_nombreServicio].precio, "No se ha enviado suficiente ether.");
+
+        // Calculamos el ether que ha sobrado de la transaccion.
+        uint256 etherSobrante_ = msg.value - servicios[_nombreServicio].precio;
+
+        // Devolvemos el ether sobrante al remitente.
+        if (etherSobrante_ > 0) {
+            payable(msg.sender).transfer(etherSobrante_);
+        }
+
+        // Agregamos el evento tomado a la mascota correspondiente.
+        serviciosTomadosPorMascota[_idMascota].push(_nombreServicio);
+
+        // Emitimos el evento correspondiente.
+        emit servicioTomado(msg.sender, _idMascota);
+
+    } 
+
+
+    function devolverServiciosTomadosPorMascota(uint256 _idMascota) public view returns(string[] memory) {
+
+        // Devolvemos los servicios que la mascota ha tomado.
+        return serviciosTomadosPorMascota[_idMascota];
+
+    }
 
 }
--- Proyecto1/contracts/subasta.sol
+++ Proyecto1/contracts/subasta.sol
@@ -29,30 +29,30 @@ contract SubastaDeObjetos {
 
 
     // Modificadores
-    modifier soloCreador(uint256 idSubasta_) {
+    modifier soloCreador(uint256 _idSubasta) {
 
         // Chequeamos que el que envia el mensaje sea el creador de la subasta.
-        require(subastas[idSubasta_].creador == msg.sender, "No eres el creador de la subasta.");
+        require(subastas[_idSubasta].creador == msg.sender, "No eres el creador de la subasta.");
 
         // Finaliza el Modifier.
         _;
 
     }
 
-    modifier soloSubastaFinalizada(uint256 idSubasta_) {
+    modifier soloSubastaFinalizada(uint256 _idSubasta) {
 
         // Chequeamos que la subasta haya finalizado.
-        require(subastas[idSubasta_].tiempoFin < block.timestamp, "La subasta no ha finalizado aun.");
+        require(subastas[_idSubasta].tiempoFin < block.timestamp, "La subasta no ha finalizado aun.");
 
         // Finaliza el Modifier.
         _;
 
     }
 
-    modifier soloSubastaActiva(uint256 idSubasta_) {
+    modifier soloSubastaActiva(uint256 _idSubasta) {
 
         // Chequeamos que la subasta no haya sido claimeada aun.
-        require(subastas[idSubasta_].estadoActiva, "La subasta ya ha sido reclamada.");
+        require(subastas[_idSubasta].estadoActiva, "La subasta ya ha sido reclamada.");
 
         // Finaliza el Modifier-
         _;
@@ -60,64 +60,64 @@ contract SubastaDeObjetos {
     }
 
 
-    function crearSubasta(string memory articulo_, string memory descripcion_, uint256 precio_, uint256 tiempoFin_) public {
+    function crearSubasta(string memory _articulo, string memory _descripcion, uint256 _precio, uint256 _tiempoFin) public {
 
         // Chequeamos que los datos ingresados son correctos.
-        require(keccak256(abi.encodePacked(articulo_)) != keccak256(abi.encodePacked("")), "El nombre no puede estar vacio.");
-        require(keccak256(abi.encodePacked(descripcion_)) != keccak256(abi.encodePacked("")), "La descripcion no puede estar vacia.");
-        require(precio_ > 0, "El precio debe ser mayor a 0.");
-        require(tiempoFin_ > 0, "El tiempo de finalizacion debe ser mayor a 0.");
+        require(keccak256(abi.encodePacked(_articulo)) != keccak256(abi.encodePacked("")), "El nombre no puede estar vacio.");
+        require(keccak256(abi.encodePacked(_descripcion)) != keccak256(abi.encodePacked("")), "La descripcion no puede estar vacia.");
+        require(_precio > 0, "El precio debe ser mayor a 0.");
+        require(_tiempoFin > 0, "El tiempo de finalizacion debe ser mayor a 0.");
 
         // Agregar nueva subasta.
-        subastas.push(Subasta(articulo_, descripcion_, precio_, tiempoFin_ + block.timestamp, msg.sender, msg.sender, true));
+        subastas.push(Subasta(_articulo, _descripcion, _precio, _tiempoFin + block.timestamp, msg.sender, msg.sender, true));
         subastasDeDireccion[msg.sender].push(subastas.length);
 
         // Emitimos el Evento Correspondiente.
-        emit nuevaSubasta(articulo_, descripcion_, precio_, tiempoFin_ + block.timestamp, msg.sender);
+        emit nuevaSubasta(_articulo, _descripcion, _precio, _tiempoFin + block.timestamp, msg.sender);
 
     }
 
 
-    function ofertarSubasta(uint idSubasta_) payable public {
+    function ofertarSubasta(uint _idSubasta) payable public {
 
         // Chequeamos que esté todo bien.
-        require(subastas[idSubasta_].tiempoFin > block.timestamp, "Subasta ya Finalizada.");
-        require(subastas[idSubasta_].precio < msg.value, "La subasta posee un valor mayor al ingresado.");
-        require(subastas[idSubasta_].ganadorActual != msg.sender, "Ya eres el mayor ofertante.");
-        require(subastas[idSubasta_].creador != msg.sender, "El creador no puede ofertar.");
+        require(subastas[_idSubasta].tiempoFin > block.timestamp, "Subasta ya Finalizada.");
+        require(subastas[_idSubasta].precio < msg.value, "La subasta posee un valor mayor al ingresado.");
+        require(subastas[_idSubasta].ganadorActual != msg.sender, "Ya eres el mayor ofertante.");
+        require(subastas[_idSubasta].creador != msg.sender, "El creador no puede ofertar.");
 
         // Chequeamos que se haya subido la oferta al nmenos una vez.
-        if(subastas[idSubasta_].ganadorActual != subastas[idSubasta_].creador) {
+        if(subastas[_idSubasta].ganadorActual != subastas[_idSubasta].creador) {
 
             // Devolvemos el Ether al ofertante anterior.
-            payable(subastas[idSubasta_].ganadorActual).transfer(subastas[idSubasta_].precio);
+            payable(subastas[_idSubasta].ganadorActual).transfer(subastas[_idSubasta].precio);
 
         }
 
         // Actualizamos el precio.
-        subastas[idSubasta_].precio = msg.value;
+        subastas[_idSubasta].precio = msg.value;
         
         // Actualizamos el mayor ofertante actual.
-        subastas[idSubasta_].ganadorActual = msg.sender;
+        subastas[_idSubasta].ganadorActual = msg.sender;
 
         // Emitimos el evento correspondiente.
-        emit nuevaOferta(idSubasta_, msg.value, msg.sender);
+        emit nuevaOferta(_idSubasta, msg.value, msg.sender);
 
     }
 
 
-    function completarSubasta(uint256 idSubasta_) public soloCreador(idSubasta_) soloSubastaFinalizada(idSubasta_) soloSubastaActiva(idSubasta_) {
+    function completarSubasta(uint256 _idSubasta) public soloCreador(_idSubasta) soloSubastaFinalizada(_idSubasta) soloSubastaActiva(_idSubasta) {
 
         // Si se desea realizar una lógica para transferir un objeto, tiene que ser antes de transferir el ether al creador de la misma.
 
         // Se desactiva la subasta antes de transferir el ether.
-        subastas[idSubasta_].estadoActiva = false;
+        subastas[_idSubasta].estadoActiva = false;
 
         // Se le transfiere el ether de la subasta al creador.
-        payable(subastas[idSubasta_].creador).transfer(subastas[idSubasta_].precio);
+        payable(subastas[_idSubasta].creador).transfer(subastas[_idSubasta].precio);
 
         // Emitimos el evento correspondiente.
-        emit ganadorDeSubasta(subastas[idSubasta_].ganadorActual, idSubasta_);
+        emit ganadorDeSubasta(subastas[_idSubasta].ganadorActual, _idSubasta);
 
     }
 
@@ -130,29 +130,29 @@ contract SubastaDeObjetos {
     }
 
 
-    function devolverSubasta(uint idSubasta_) public view returns(Subasta memory) {
+    function devolverSubasta(uint _idSubasta) public view returns(Subasta memory) {
 
         // Devolvemos la subasa en el indice.
-        return subastas[idSubasta_];
+        return subastas[_idSubasta];
 
     }
 
 
-    function devolverSubastasDeDireccion(address direccion_) public view returns(uint[] memory) {
+    function devolverSubastasDeDireccion(address _direccion) public view returns(uint[] memory) {
 
         // Devolvemos el array enlazado con la direccion en el mapping.
-        return subastasDeDireccion[direccion_];
+        return subastasDeDireccion[_direccion];
 
     }
 
 
-    function tiempoRestanteSubasta(uint256 idSubasta_) public view returns(uint) {
+    function tiempoRestanteSubasta(uint256 _idSubasta) public view returns(uint) {
 
         // Devolvemos el tiempo restante, si es que no ha finalizado.
-        if (subastas[idSubasta_].tiempoFin < block.timestamp) {
+        if (subastas[_idSubasta].tiempoFin < block.timestamp) {
             return 0;
         } else {
-            return subastas[idSubasta_].tiempoFin - block.timestamp;
+            return subastas[_idSubasta].tiempoFin - block.timestamp;
         }
 
     }
