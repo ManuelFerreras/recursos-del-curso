790c6ab32f2808ff3ddfe75c1a69838a9616473f
Manuel Ferreras <manuel.ferreras1@gmail.com>
2022-01-11 21:40:24 -0300

    Metadata

--- Proyecto 6/contracts/nft.sol
+++ Proyecto 6/contracts/nft.sol
@@ -1,10 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contract/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract nfts {
+contract nfts is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
 
+    string baseURI;
 
+    constructor(string memory _nombre, string memory _simbolo) ERC721(_nombre, _simbolo) {}
 
+    function mint()
+        public
+        returns (uint256)
+    {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, baseURI);
+
+        return newItemId;
+        print("Hola Mundo");
+    }
 }
